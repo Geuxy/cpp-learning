1d7c14f669c66f281886f9be6bcdcd17f1044618
Geuxy <88702612+geuxy@users.noreply.github.com>
2022-09-16 20:50:12 +0800

    Add files via upload

---
+++ filechecker.cpp
@@ -0,0 +1,23 @@
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+    std::fstream file;
+    file.open(argv[1]);
+    
+    if(argc > 2 || argc < 2) {
+        std::cout << "Invalid arguments!" << std::endl;
+        return 0;
+
+    } else {
+        if(file.is_open()) {
+            std::cout << "File exists!" << std::endl;
+            return 1;
+        } else {
+            std::cout << "File doesnt exist is unreadable!" << std::endl;
+            return 0;
+        }
+    }
+
+}
